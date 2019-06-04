c33df4b4817142faaf9a6cc3cd327463a96717ba
blueskyadd <tian9711@126.com>
2019-06-04 19:14:34 +0800

    导航栏调整，图片名称修改

--- wlht/src/components/layout/layout.vue
+++ wlht/src/components/layout/layout.vue
@@ -7,11 +7,12 @@
       <div class="layout_nav_box">
         <nav>
           <ul>
-              <router-link to="memberManagement" tag="li" activeactive-class="active">
+              <router-link :to="item.link" tag="li" activeactive-class="active" v-for="(item, index) in tabList" :key="index">
                 <span>
-                  <img src="../../assets/img/1.png" alt="">
+                  <img :src="item.image_normal" alt="" :class="{'active': item.link == $route.path}">
+                  <img :src="item.image_actively" alt="" :class="{'active': item.link != $route.path}">
                 </span>
-                <p>商品管理</p>
+                <p>{{item.name}}</p>
               </router-link>
           </ul>
         </nav>
@@ -27,7 +28,64 @@
 
 <script>
     export default {
-        name: "layout"
+        name: "layout",
+        data(){
+          return{
+            tabList:[
+              {
+                name: '商品管理',
+                image_normal:require('../../assets/img/member_normal.png'),
+                image_actively: require('../../assets/img/member_actively.png'),
+                link: '/memberManagement'
+              },
+              {
+                name: '非会员管理',
+                image_normal:require('../../assets/img/nomember_normal.png'),
+                image_actively: require('../../assets/img/nomember_actively.png'),
+                link: '/index'
+              },
+              {
+                name: '课程管理',
+                image_normal:require('../../assets/img/yx_normal.png'),
+                image_actively: require('../../assets/img/yx_actively.png'),
+                link: '/index'
+              },
+              {
+                name: '课表管理',
+                image_normal:require('../../assets/img/shop_normal.png'),
+                image_actively: require('../../assets/img/shop_actively.png'),
+                link: '/index'
+              },
+              {
+                name: '比赛管理',
+                image_normal:require('../../assets/img/bs_normal.png'),
+                image_actively: require('../../assets/img/bs_actively.png'),
+                link: '/index'
+              },
+              {
+                name: '主推热点',
+                image_normal:require('../../assets/img/rd_normal.png'),
+                image_actively: require('../../assets/img/rd_actively.png'),
+                link: '/index'
+              },
+              {
+                name: '场地管理',
+                image_normal:require('../../assets/img/cd_normal.png'),
+                image_actively: require('../../assets/img/cd_actively.png'),
+                link: '/index'
+              },
+              {
+                name: '任职人员管理',
+                image_normal:require('../../assets/img/user_normal.png'),
+                image_actively: require('../../assets/img/user_actively.png'),
+                link: '/index'
+              }
+            ]
+          }
+        },
+        mounted() {
+          console.log(this.$route.path)
+        },
     }
 </script>
 
@@ -39,6 +97,7 @@
 .el-aside{
   width: 3.2rem !important;
   background: #fff;
+  
   .layout_header_logo{
     img{
       width: 100%;
@@ -48,14 +107,27 @@
   .layout_nav_box{
     padding: .81rem 0 0 0;
     font-size: .2rem;
+    .active{
+        display: none;
+      }
+    li{
+      border-left: 0.05rem solid #fff;
+
+    }
     .router-link-exact-active{
       background: #f2effe;
       border-left: 0.05rem solid #7F63F4;
       color: #7F63F4;
+      
+      .no_active{
+        // display: 
+      }
     }
     li{
       line-height: .56rem;
+      font-size: .16rem;
       height: .56rem;
+      color: #888d98;
       padding-left: .44rem;
       span{
         float: left;
