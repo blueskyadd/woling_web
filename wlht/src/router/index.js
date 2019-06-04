cb566afe569c9cfa7bf05c06361dbba501d9133b
Majiarui <1581699719@example.com>
2019-06-04 14:54:10 +0800

    "模板提交"

--- wlht/src/router/index.js
+++ wlht/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Layout from '../components/layout/layout'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { 
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Layout',
+      component: Layout,
+      children:[
+        {
+          path: '/index',
+          name: 'index',
+          component:() => import('../view/index/index'),
+        }
+      ]
+    },
   ]
 })
---
+++ wlht/src/view/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-main>
+    ss
+  </el-main>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped lang="scss">
+  .el-main{
+    padding: 0;
+  }
+</style>
