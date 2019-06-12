d3f9a85ed2e1d03ed2992194aa70be436c776bf0
Majiarui <1581699719@example.com>
2019-06-12 16:49:33 +0800

    "提交"

--- wlht/src/store/index.js
+++ wlht/src/store/index.js
@@ -1,17 +1,19 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-    // 内存数据状态，UI可通过this.$store.state.* 获得数据
-    state : {
-        isEditOut: true
-    }, //新加的
-    mutations : {
-        changeisEditOut: (state, flag) => {
-            state.isEditOut = flag
-            console.log(flag)
-        },
+const store = new Vuex.Store({
+  state : {
+    isEditOut: true
+  }, //新加的
+  mutations : {
+    changeisEditOut: (state, flag) => {
+      state.isEditOut = flag
+      console.log(flag)
     },
+  },
+
 })
+
+export default store
